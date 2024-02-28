60e87b58fa2b450b4f52814d274ee3e8fd3fbb83
Moe Ayoub <mma.ayoub@outlook.com>
2024-02-28 15:53:59 -0600

    Crew, Genres, Header, & Image Wrapper Components
    This is mainly for the details component in the main page(s)

---
+++ components/CrewCategory.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+    title: string,
+    people: Person[],
+}>()
+</script>
+<template>
+    <div v-if="people.length > 0" class="flex gap-2 flex-wrap">
+        <p class="font-semibold">
+            {{ title }}
+        </p>
+        <NuxtLink v-for="p in people" :key="p.id" :to="{ path: '/people/' + p.id }" class="after:content-['↗']">
+            {{ p.name }}
+        </NuxtLink>
+    </div>
+</template>
---
+++ components/details/Img.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const prop = withDefaults(defineProps<{
+    imgPath?: string
+    type: MediaType | 'person'
+    externalIds?: ExternalIds
+}>(), {
+    type: 'movie'
+})
+
+function imgSrc(): string {
+    if (prop.type == 'person')
+        return tmdbImage(prop.imgPath, TmdbProfileSizes.og)
+    else
+        return tmdbImage(prop.imgPath, TmdbPosterSizes.md)
+}
+
+const externalLinks = ref<ExternalLink[]>([]);
+
+if (prop.externalIds) {
+    Object.entries(prop.externalIds).forEach(([key, value]) => {
+        if (key.toLocaleLowerCase().includes('imdb') && value) {
+            const externalLink: ExternalLink = {
+                Link: value.includes('nm') ? 'https://www.imdb.com/name/' + value : 'https://www.imdb.com/title/' + value,
+                IconName: 'bxl:imdb'
+            }
+
+            externalLinks.value.push(externalLink);
+        }
+
+        if (key.toLocaleLowerCase().includes('twitter') && value) {
+            const externalLink: ExternalLink = {
+                Link: 'https://twitter.com/' + value,
+                IconName: 'bxl:twitter'
+            }
+            externalLinks.value.push(externalLink);
+        }
+
+        if (key.toLocaleLowerCase().includes('instagram') && value) {
+            const externalLink: ExternalLink = {
+                Link: 'https://www.instagram.com/' + value,
+                IconName: 'bxl:instagram-alt'
+            }
+
+            externalLinks.value.push(externalLink);
+        }
+
+        if (key.toLocaleLowerCase().includes('homepage') && value) {
+            const externalLink: ExternalLink = {
+                Link: value,
+                IconName: 'mdi:link-box-variant'
+            }
+
+            externalLinks.value.push(externalLink);
+        }
+    })
+}
+</script>
+
+<template>
+    <div class="hidden sm:flex sm:flex-col divide-y-4 divide-secondary dark:divide-primary">
+        <NuxtImg class="h-96 w-full" :src="imgSrc()" :class="{ '-mt-16': type != 'person', 'rounded-t-xl': externalLinks.length > 0, 'rounded-xl': externalLinks.length == 0 }" />
+        <ExternalLinks v-if="externalLinks.length > 0" :external-links="externalLinks" :type="type" />
+    </div>
+</template>
