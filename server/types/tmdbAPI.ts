794b4475bc758acd70d0b1176a056cedcfae1cb0
Moe Ayoub <mma.ayoub@outlook.com>
2024-02-28 15:40:29 -0600

    Adjusting Proxy API for performance improvements

--- server/types/tmdbAPI.ts
+++ server/types/tmdbAPI.ts
@@ -1,8 +1,17 @@
 export class TmdbAPI {
     api;
 
-    constructor() {
+    constructor(credits: boolean = true, AV: boolean = false) {
         const config = useRuntimeConfig();
+        let appendToResponse = 'combined_credits,content_ratings,external_ids,release_dates'
+
+        if (credits)
+            appendToResponse += ',credits'
+
+
+        if (AV)
+            appendToResponse += ',videos,images'
+
         this.api = $fetch.create({
             baseURL: config.public.tmdbBase,
             headers: {
@@ -14,7 +23,8 @@ export class TmdbAPI {
             params: {
                 page: 1,
                 region: 'US',
-                language: 'en-US'
+                language: 'en',
+                append_to_response: appendToResponse
             }
         })
     }
