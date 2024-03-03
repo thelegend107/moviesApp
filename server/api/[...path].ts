cb16bdbacf16520d692d2427164476751edefc47
Moe Ayoub <mma.ayoub@outlook.com>
2024-03-03 15:10:10 -0600

    Merge branch 'Base-Layer' into Development-1

--- server/api/[...path].ts
+++ server/api/[...path].ts
@@ -2,15 +2,30 @@ export default defineEventHandler((event) => {
     let tmdbPath: string = ''
     const query = getQuery(event)
 
+    const tmdb = new TmdbAPI((query.credits === 'true'), (query.videos === 'true'), (query.images === 'true'), (query.aggregate_credits === 'true'))
     if (event.context.params?.path) tmdbPath = event.context.params.path
+    else throw createError({
+        statusCode: 404,
+        statusMessage: `Not Found: API route: "${event.path}" was not found`
+    })
 
-    return tmdbPath
+    try {
+        return tmdb.api(tmdbPath, {
+            method: 'GET',
+        })
+    }
+    catch (e: any) {
+        const status = e?.response?.status || 500
+        setResponseStatus(event, status)
+        return {
+            error: e,
+        }
+    }
 })
 
 // export default defineEventHandler(async (event) => {
 //     let tmdbPath: string = ''
 //     const query = getQuery(event)
-//     const tmdb = new TmdbAPI((query.credits === 'true'), (query.videos === 'true'), (query.images === 'true'), (query.aggregate_credits === 'true'))
 
 
 //     else throw createError({
