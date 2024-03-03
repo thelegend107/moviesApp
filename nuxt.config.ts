1178f120453006e8809a2e28827717cd97661939
Moe Ayoub <mma.ayoub@outlook.com>
2024-03-03 12:45:34 -0600

    Merge branch 'Base-Layer' into Development-1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     nitro: {
-        preset: "azure-functions",
+        preset: "azure",
         imports: {
             dirs: ['server', 'server/middleware/**', 'server/types/**'],
         }
